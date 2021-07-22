881c597be46cdacf905c349c2df7163fb18f7b57
Chris0512 <kyang3200@gmail.com>
2021-07-22 11:52:11 +0800

    Initial commit

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,25 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Sum of digits in number 125 is " + sumDigits(125));
+    }
+
+    private static int sumDigits(int number){
+
+        if(number < 10) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        while(number > 0){
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        return sum;
+
+    }
+}
